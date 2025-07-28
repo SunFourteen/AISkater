bb5b24b50fc31fffb10f0c989e3eca20002ca688
SunFourteen <sunfourteen@163.com>
2025-07-28 16:18:33 +0800

    v1 Seq2Seq

--- v1/model_v1.py
+++ v1/model_v1.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.utils.rnn as rnn_utils
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset, random_split
-from tqdm import tqdm
 import math
 from datetime import datetime
 from load_data import Load_Data
