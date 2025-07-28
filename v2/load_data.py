355bd40d601688faa566a26f92f450aac06d3778
SunFourteen <sunfourteen@163.com>
2025-07-28 16:18:50 +0800

    v2 SeqEmbedding

---
+++ v2/load_data.py
@@ -0,0 +1,339 @@
+import numpy as np
+import random
+import math
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+class Load_Data():
+    
+    def __init__(self, word_path):
+        
+        self.word_path = word_path
+        self.key_seq_fault_rate = 0.1
+
+        self.anchor, self.anchor_size, self.anchor_max_length  = self.load_triplet_data()
+        print(f'Anchor Generation Done! \nSize: {self.anchor_size} \nMax Length: {self.anchor_max_length} ')
+        self.positive, self.positive_size, self.positive_max_length = self.load_triplet_data()
+        print(f'Positve Generation Done! \nSize: {self.positive_size} \nMax Length: {self.positive_max_length} ')
+        self.negative, self.negative_size, self.negative_max_length = self.load_triplet_data()
+        random.shuffle(self.negative)
+        print(f'Negative Generation Done! \nSize: {self.negative_size} \nMax Length: {self.negative_max_length} ')
+       
+        assert (self.anchor_size == self.positive_size and self.anchor_size == self.negative_size)
+
+
+    def load_triplet_data(self):
+
+        words = self.load_words(self.word_path)
+        # print(words[0])
+
+        key_seqs = self.load_key_seqs(words)
+        # print(key_seqs[0])
+
+        disturbanced_key_seqs = self.load_disturbanced_key_seqs(key_seqs)
+        # print(disturbanced_key_seqs[0])
+
+        twist_seqs = self.load_twist_seqs(disturbanced_key_seqs)
+        # print(twist_seqs[0])
+
+        spacial_seqs, timely_seqs, masks = self.load_seqs(twist_seqs)
+        # print(spacial_seqs[0])
+        # print(timely_seqs[0])
+
+        # for seq in spacial_seqs:
+        #     self.show_seq(seq)
+        #     print(len(seq))
+        # for seq in timely_seqs:
+        #     self.show_seq(seq)
+        #     print(len(seq))
+
+        data = self.load_data(timely_seqs, words)
+        # print(data[0])
+        data_size = len(data)
+        # print(data_size')
+        max_length = max(len(data[i][0]) for i in range(data_size))
+        # print(self.max_length)
+
+        for i in range(data_size):
+            for j in range(len(data[i][0])):
+                if len(data[i][0][j]) != 6:
+                    print('ERROR! position: {i} {j}')
+        
+        return data, data_size, max_length
+        
+        
+    def load_words(self, word_path):
+        
+        with open(word_path, "r") as f:
+            words = f.readlines()
+        self.data_size = len(words)
+
+        print('Loading Words Done! ')
+        return words # list[string]
+    
+    
+    def load_key_seqs(self, words):
+
+        key_seqs = []
+        for word in tqdm(words):
+            if '\n' in word:
+                 word = word[:-1]
+            key_seq = []
+            for letter in word:
+                key_pair = self.letter2key(letter)
+                key_seq.extend(key_pair)
+            key_seq = np.array(key_seq)
+            key_seq = key_seq[np.where(key_seq[:-1] != key_seq[1:])[0]].tolist() + [int(key_seq[-1])]
+            key_seqs.append(key_seq)
+
+        print('Load Key Sequences Done! ')
+        return key_seqs # list[list[int]]
+    
+    
+    def load_disturbanced_key_seqs(self, key_seqs):
+        
+        indices = random.sample(range(self.data_size), int(self.data_size * self.key_seq_fault_rate))
+        for idx in indices:
+            key_seqs[idx] = self.key_disturbancer(key_seqs[idx])
+        indices = random.sample(range(self.data_size), int(self.data_size * self.key_seq_fault_rate ** 2))
+        for idx in indices:
+            key_seqs[idx] = self.key_disturbancer(key_seqs[idx])
+            key_seqs[idx] = self.key_disturbancer(key_seqs[idx])
+        
+        print('Load Disturbanced Key Sequences Done! ')
+        return key_seqs # list[list[int]]
+    
+    
+    def load_twist_seqs(self, key_seqs):
+        
+        twist_seqs = []
+        for key_seq in tqdm(key_seqs):
+            twist_seq = []
+            for key in key_seq:
+                twist_seq.append(self.select_twist(key))
+            twist_seqs.append(twist_seq)
+
+        print('Load Twist Sequences Done! ')
+        return twist_seqs # list[list[(x, y)]]
+
+    
+    def load_seqs(self, twist_seqs):
+        
+        disturbance_radius = 0.02
+        sample_delta = 0.02
+        concentrate_factor = 0.2
+        concentrate_sample_num = 3
+        sparse_sample_num = 4
+        spacial_seqs = []
+        masks = []
+        timely_seqs = []
+
+        for twist_seq in tqdm(twist_seqs):
+            combined_spacial_seq = []
+            mask = []
+            combined_timely_seq = []
+            for idx in range(len(twist_seq) - 1):
+
+                spacial_seq = []
+                timely_seq = []
+                delta_x = twist_seq[idx + 1][0] - twist_seq[idx][0]
+                delta_y = twist_seq[idx + 1][1] - twist_seq[idx][1]
+                delta = np.linalg.norm((delta_x, delta_y))
+
+                sample_num = int(delta // sample_delta)
+                x_seq = np.linspace(twist_seq[idx][0], twist_seq[idx + 1][0], sample_num)[:-1].tolist()
+                y_seq = np.linspace(twist_seq[idx][1], twist_seq[idx + 1][1], sample_num)[:-1].tolist()
+                for i in range(sample_num - 1):
+                    coodinate = (x_seq[i], y_seq[i])
+                    coodinate = self.point_disturbancer(coodinate, disturbance_radius)
+                    spacial_seq.append(coodinate)
+                combined_spacial_seq.extend(spacial_seq)
+
+                mask.append(1)
+                for i in range(sample_num - 2):
+                    mask.append(0)
+
+                concentrate_delta_x = delta_x * concentrate_factor
+                concentrate_delta_y = delta_y * concentrate_factor
+                sub1_x = twist_seq[idx][0] + concentrate_delta_x
+                sub1_y = twist_seq[idx][1] + concentrate_delta_y
+                sub2_x = twist_seq[idx + 1][0] - concentrate_delta_x
+                sub2_y = twist_seq[idx + 1][1] - concentrate_delta_y
+                x_seq = []
+                y_seq = []
+                x_seq1 = np.linspace(twist_seq[idx][0], sub1_x, concentrate_sample_num)[:-1].tolist()
+                y_seq1 = np.linspace(twist_seq[idx][1], sub1_y, concentrate_sample_num)[:-1].tolist()
+                x_seq2 = np.linspace(sub1_x, sub2_x, sparse_sample_num)[:-1].tolist()
+                y_seq2 = np.linspace(sub1_y, sub2_y, sparse_sample_num)[:-1].tolist()
+                x_seq3 = np.linspace(sub2_x, twist_seq[idx + 1][0], concentrate_sample_num)[:-1].tolist()
+                y_seq3 = np.linspace(sub2_y, twist_seq[idx + 1][1], concentrate_sample_num)[:-1].tolist()
+                x_seq.extend(x_seq1)
+                x_seq.extend(x_seq2)
+                x_seq.extend(x_seq3)
+                y_seq.extend(y_seq1)
+                y_seq.extend(y_seq2)
+                y_seq.extend(y_seq3)
+                for i in range(concentrate_sample_num * 2 + sparse_sample_num - 3):
+                    coodinate = (x_seq[i], y_seq[i])
+                    coodinate = self.point_disturbancer(coodinate, disturbance_radius)
+                    timely_seq.append(coodinate)
+                combined_timely_seq.extend(timely_seq)
+                
+            combined_spacial_seq.append((twist_seq[-1][0], twist_seq[-1][1]))
+            combined_timely_seq.append((twist_seq[-1][0], twist_seq[-1][1]))
+            mask.append(1)
+
+            spacial_seqs.append(combined_spacial_seq)
+            timely_seqs.append(combined_timely_seq)
+            masks.append(mask)
+
+        print('Load Sequences Done! ')
+        return spacial_seqs, timely_seqs, masks # list[list[(x, y)]], list[list[(x, y)]] list[list[int]]
+        # masks are for spacial_seqs
+
+
+    def load_data(self, timely_seqs, words):
+        
+        dataset = []
+
+        for idx in tqdm(range(self.data_size)):
+            data = []
+            timely_seq = timely_seqs[idx]
+            word = words[idx]
+            if '\n' in word:
+                word = word[:-1]
+            length = len(timely_seq)
+            
+            if length < 3:
+                print(f'Skip Word with Short Sequence: Word {word} Length {length}')
+                continue
+            
+            for i in range(1, length-1):
+                x_prev, y_prev = timely_seq[i - 1]
+                x, y = timely_seq[i]
+                x_next, y_next= timely_seq[i + 1]
+                dx = (x_next - x_prev) * 3
+                dy = (y_next - y_prev) * 3
+                vec_prev = np.array([x - x_prev, y - y_prev])
+                vec_next = np.array([x_next - x, y_next - y])
+                norm_prev = max(np.linalg.norm(vec_prev), 1e-8)
+                norm_next = max(np.linalg.norm(vec_next), 1e-8)
+                cos_theta = np.dot(vec_prev, vec_next) / (norm_prev * norm_next)
+                cos_theta = np.clip(cos_theta, -1.0, 1.0)
+                dtheta = np.acos(cos_theta) / np.pi
+                dt = 1
+                data.append((x, y, dx, dy, dtheta, dt)) # normalization
+                
+            if len(data) > 0:
+                dataset.append((data, word))
+            else:
+                print(f"Skip Word with Empty Sequence: Word {word}")
+            
+        print('Load Data Done! ')
+        return dataset # list[(list(x, y, dx, dy, dtheta, dt), word)]
+
+
+    def letter2key(self, letter):
+
+        key_map = {
+                'a': (1, 1), 'b': (1, 2), 'c': (1, 3), 'd': (1, 4), 'e': (1, 5),
+                'f': (2, 1), 'g': (2, 2), 'h': (2, 3), 'i': (2, 4), 'j': (2, 5),
+                'k': (3, 1), 'l': (3, 2), 'm': (3, 3), 'n': (3, 4), 'o': (3, 5),
+                'p': (4, 1), 'q': (4, 2), 'r': (4, 3), 's': (4, 4), 't': (4, 5),
+                'u': (5, 1), 'v': (5, 2), 'w': (5, 3), 'x': (5, 4), 'y': (5, 5),
+                'z': (5, 5)
+            }
+        
+        return key_map.get(letter.lower())
+    
+    
+    def key_disturbancer(self, key_seq):
+        
+        if len(key_seq) == 1:
+            return key_seq
+        key_seq = np.array(key_seq)
+        indice = np.random.randint(0, len(key_seq))
+        if indice == 0:
+            mask = ((np.arange(1, 7) != key_seq[1]))
+        elif indice == len(key_seq):
+            mask = ((np.arange(1, 7) != key_seq[-1]))
+        else:
+            mask = (np.arange(1, 7) != key_seq[indice-1]) & (np.arange(1, 7) != key_seq[indice])
+        candidates = np.arange(1, 7)[mask] if mask.any() else np.arange(1, 7)
+        insert_num = int(np.random.choice(candidates))
+        key_seq = key_seq.tolist()
+        disturbanced_key_seq = np.concatenate([key_seq[:indice], [insert_num], key_seq[indice:]]).tolist()
+        disturbanced_key_seq =[int(x) for x in disturbanced_key_seq]
+        
+        return disturbanced_key_seq
+    
+    
+    def select_twist(self, key):
+        
+        radius = 0.25
+        disturbance_radius = 0.05
+        concentration_factor = 0.3
+
+        if key == 3:
+            r = 1
+            while r > radius:
+                r = abs(random.gauss(0, concentration_factor)) * radius
+            theta = random.gauss(0, concentration_factor) * math.pi * 2
+            x = r * math.cos(theta)
+            y = r * math.sin(theta)
+            coodinate = (0.5 + x, 0.5 + y)
+        else:
+            x = 0.5
+            y = 0.5
+            while (x - 0.5) ** 2 + (y - 0.5) ** 2 < radius ** 2 or x > 0.5 or y > 0.5:
+                x = abs(random.gauss(0, concentration_factor)) * 0.25 + 0.15
+                y = abs(random.gauss(0, concentration_factor)) * 0.25 + 0.15
+            if key == 1:
+                coodinate = (x, y)
+            elif key == 2:
+                coodinate = (1 - x, y)
+            elif key == 4:
+                coodinate = (x, 1 - y)
+            else:
+                coodinate = (1 - x, 1 - y)
+        coodinate = self.point_disturbancer(coodinate, disturbance_radius)
+
+        return coodinate
+    
+    
+    def point_disturbancer(self, coodinate, max_radius):
+
+        x, y = coodinate
+        r = random.random() * max_radius
+        theta = random.random() * math.pi * 2
+        x = x + r * math.cos(theta)
+        y = y + r * math.sin(theta)
+        if x < 0:
+            x = 0.0
+        if y < 0:
+            y = 0.0
+        if x > 1:
+            x = 1.0
+        if y > 1:
+            y = 1.0
+        coodinate = (x, y)
+
+        return coodinate
+
+    
+    def show_seq(self, seq):
+        
+        x = [seq[i][0] for i in range(len(seq))]
+        y = [seq[i][1] for i in range(len(seq))]
+        plt.plot(x, y, marker='o', markersize = 1, linestyle='solid')
+        plt.xlim(0, 1)
+        plt.ylim(1, 0)
+        plt.show()
+        
+        return
+
+
+
+if __name__ == '__main__':
+    data_path = "puma.txt"
+    data = Load_Data(data_path)
---
+++ v2/seq_embedding.py
@@ -0,0 +1,378 @@
+import torch
+import torch.nn as nn
+import torch.nn.utils.rnn as rnn_utils
+import torch.nn.functional as F
+import numpy as np
+
+from scipy.spatial.distance import cdist
+from torch.utils.data import Dataset, DataLoader
+
+from tqdm import tqdm
+from datetime import datetime
+import time
+from load_data import Load_Data
+
+class SequentialEmbedder(nn.Module):
+    
+    '''
+    Boidirectional GRU
+
+    INPUT
+    src: (batch_size, max_seq_len, input_dim)  
+    src_lens: (batch_size, )
+
+    OUTPUT
+    embedding: (batch_size, embedding_dim)
+    '''
+    
+    def __init__(self, input_dim, hidden_dim, embedding_dim, 
+                 num_layers=2, num_directions=2, dropout=0.1):
+        
+        super().__init__()
+        
+        self.rnn = nn.GRU(
+            input_size = input_dim, 
+            hidden_size = hidden_dim, 
+            num_layers = num_layers, 
+            bidirectional = (num_directions == 2), 
+            batch_first = True, 
+            dropout = dropout if num_layers > 1 else 0
+        )
+
+        self.adaptive_pool = nn.AdaptiveAvgPool1d(1)
+
+        self.embedding = nn.Sequential(
+            nn.Linear(hidden_dim * num_layers * num_directions, hidden_dim * num_layers), 
+            nn.ReLU(), 
+            nn.Dropout(dropout), 
+            nn.Linear(hidden_dim * num_layers, embedding_dim)
+        )
+        
+    
+    def forward(self, src, src_lens):
+        
+        # self.rnn:
+        # INPUT
+        # src: (batch_size, max_seq_len, input_dim)
+        # OUTPUT
+        # output: (batch_size, max_seq_len, hidden_dim * num_directions)
+        # hidden: (num_layers * num_directions, batch_size, hidden_dim)
+
+        # self.projection:
+        # INPUT   hidden: (batch_size, hidden_dim * num_layers * num_directions, )
+        # OUTPUT  embedding:(batch_size, embedding_dim)
+
+        # num_layers = 2
+        # num_directions = 1 if 单向 else 2
+
+        packed_src = rnn_utils.pack_padded_sequence(
+            src, src_lens.cpu(), batch_first=True, enforce_sorted=False
+            )
+        # PackedSequence class
+        packed_outputs, hidden = self.rnn(packed_src)
+        # PackedSequence class, (nun_layers * num_directions, batch_size, hidden_dim)
+        output, _ = nn.utils.rnn.pad_packed_sequence(
+            packed_outputs, batch_first=True
+        )
+        # (batch_size, max_seq_len, hidden_dim * num_directions)
+
+        pooled = self.adaptive_pool(output.permute(0, 2, 1)).squeeze(-1)
+        # (batch_size, hidden_dim * num_directions)
+
+        hidden_forward = hidden[-2, :, :]
+        # (batch_size, hidden_dim)
+        hidden_backward = hidden[-1, :, :]
+        # (batch_size, hidden_dim)
+
+        combined = torch.cat((pooled, hidden_forward, hidden_backward), dim=-1)
+        # (batch_size, hidden_dim * nun_layers * num_directions)
+
+        embedding = self.embedding(combined)
+        # (batch_size, embedding_dim)
+        
+        return embedding # (batch_size, embedding_dim)
+
+
+
+class SequenceSimilarity():
+
+    def __init__(self, metric='DTW'):
+        self.metric = metric
+
+    
+    def compute(self, seq1, seq2):
+
+        seq1, seq2 = self.feature(seq1, seq2)
+        if self.metric == 'euclidean':
+            return self.euclidian(seq1, seq2)
+        elif self.metric == 'cosine':
+            return self.cosine(seq1, seq2)
+        elif self.metric == 'DTW':
+            return self.dynamic_time_warping(seq1, seq2)
+        else:
+            raise ValueError(f"Unsupported metric: {self.metric}")
+
+
+    def feature(self, seq1, seq2):
+
+        return seq1, seq2
+
+
+    def euclidian(self,seq1, seq2):
+
+        return np.exp(-cdist(seq1, seq2).mean())
+
+
+    def cosine(self, seq1, seq2):
+
+        flat1 = seq1.flatten()
+        flat2 = seq2.flatten()
+        min_len = min(len(flat1), len(flat2))
+        vec1 = flat1[:min_len]
+        vec2 = flat2[:min_len]
+        dot = np.dot(vec1, vec2)
+        norm1 = np.linalg.norm(vec1)
+        norm2 = np.linalg.norm(vec2)
+        cosine = dot / (norm1 * norm2)
+
+        return cosine
+
+
+    def dynamic_time_warping(self, seq1, seq2):
+
+        n = len(seq1)
+        m = len(seq2)
+        dtw_matrix = np.zeros((n+1, m+1))
+        dtw_matrix[0, 1:] = np.inf
+        dtw_matrix[1:, 0] = np.inf
+        
+        dist_matrix = cdist(seq1, seq2, 'euclidean')
+        
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                cost = dist_matrix[i-1, j-1]
+                dtw_matrix[i, j] = cost + min(
+                    dtw_matrix[i-1, j], 
+                    dtw_matrix[i, j-1], 
+                    dtw_matrix[i-1, j-1]
+                )
+        
+        return np.exp(-dtw_matrix[n, m] / max(n, m))
+
+
+
+class ContractiveLoss(nn.Module):
+
+    def __init__(self, margin, similarity_weight):
+
+        super().__init__()
+        self.margin = margin
+        self.similarity_weight = similarity_weight
+
+    
+    def forward(self, anchor, positive, negative, similarity_matrix):
+
+        distance_pos = F.pairwise_distance(anchor, positive, 2) # (batch_size, )
+        distance_neg = F.pairwise_distance(anchor, negative, 2) # (batch_size, )
+        triplet_loss = F.relu(distance_pos - distance_neg + self.margin, 0) # (batch_size, )
+
+        batch_size = anchor.size(0)
+        similarity_loss = 0
+
+        for i in range(batch_size):
+            for j in range(batch_size):
+
+                emb_similarity = F.cosine_similarity(anchor[i].unsqueeze(0), anchor[j].unsqueeze(0))
+                similarity_loss += F.mse_loss(emb_similarity, similarity_matrix[i, j])
+        
+        similarity_loss /= (batch_size * batch_size)
+        
+        total_loss = triplet_loss.mean() + self.similarity_weight * similarity_loss
+
+        return total_loss
+
+
+
+class SequenceDataset(Dataset):
+
+    def __init__(self, file_path):
+        
+        self.load_data = Load_Data(file_path)
+        self.anchor = self.load_data.anchor
+        self.positive = self.load_data.positive
+        self.negative = self.load_data.negative
+        self.size = self.load_data.anchor_size
+    
+
+    def __len__(self):
+
+        return self.size
+    
+
+    def __getitem__(self, idx):
+
+        anchor_data = self.anchor[idx]
+        positive_data = self.positive[idx]
+        negative_data = self.negative[idx]
+
+        anchor_seq = anchor_data[0]
+        positive_seq = positive_data[0]
+        negative_seq = negative_data[0]
+        
+        return (
+            torch.tensor(anchor_seq, dtype=torch.float32),
+            torch.tensor(positive_seq, dtype=torch.float32),
+            torch.tensor(negative_seq, dtype=torch.float32)
+        )
+
+
+
+def collate_triplet_fn(batch):
+
+    anchor_seqs, positive_seqs, negative_seqs = zip(*batch)
+    
+    anchor_lengths = torch.tensor([len(seq) for seq in anchor_seqs])
+    positive_lengths = torch.tensor([len(seq) for seq in positive_seqs])
+    negative_lengths = torch.tensor([len(seq) for seq in negative_seqs])
+    
+    padded_anchor = rnn_utils.pad_sequence(anchor_seqs, batch_first=True)
+    padded_positive = rnn_utils.pad_sequence(positive_seqs, batch_first=True)
+    padded_negative = rnn_utils.pad_sequence(negative_seqs, batch_first=True)
+    
+    return (
+        padded_anchor, anchor_lengths, 
+        padded_positive, positive_lengths, 
+        padded_negative, negative_lengths
+    )
+
+
+def compute_similarity_matrix(sequences, metric='DTW'):
+
+    n = len(sequences)
+    similarity_matrix = np.zeros((n, n))
+    calculator = SequenceSimilarity(metric=metric)
+    
+    print(f'Calculate Similarity Matrix --Type: {metric}')
+    for i in tqdm(range(n)):
+        for j in range(i, n):
+            if i == j:
+                similarity_matrix[i, j] = 0.0
+            else:
+                similarity = calculator.compute(sequences[i], sequences[j])
+                similarity_matrix[i, j] = similarity
+                similarity_matrix[j, i] = similarity
+    
+    return torch.tensor(similarity_matrix, dtype=torch.float32)
+
+
+
+def train(model, dataloader, optimizer, criterion, device, epochs=20):
+    
+    model.train()
+
+    for epoch in range(epochs):
+
+        epoch_loss = 0
+        batch_num = len(dataloader)
+
+        for batch_idx, batch in enumerate(dataloader):
+
+            padded_anchors, anchor_lengths, \
+            padded_positives, positive_lengths, \
+            padded_negatives, negative_lengths = batch
+
+            padded_anchors = padded_anchors.to(device)
+            anchor_lengths = anchor_lengths.to(device)
+            padded_positives = padded_positives.to(device)
+            positive_lengths = positive_lengths.to(device)
+            padded_negatives = padded_negatives.to(device)
+            negative_lengths = negative_lengths.to(device)
+
+            anchors = []
+            for i, anchor in enumerate(padded_anchors):
+                
+                length = anchor_lengths[i]
+                anchors.append(anchor[:length].cpu().numpy())
+
+            similarity_matrix = compute_similarity_matrix(anchors, metric='DTW')
+            similarity_matrix = similarity_matrix.to(device)
+
+            optimizer.zero_grad()
+            anchor_emb = model(padded_anchors, anchor_lengths)
+            positive_emb = model(padded_positives, positive_lengths)
+            negative_emb = model(padded_negatives, negative_lengths)
+            
+            loss = criterion(anchor_emb, positive_emb, negative_emb, similarity_matrix)
+            
+            loss.backward()
+            optimizer.step()
+            
+            epoch_loss += loss.item()
+            print(f'Eopch[{epoch+1}/{epochs}] Batch[{batch_idx+1}/{batch_num}] Loss: {loss}')
+        
+        epoch_ave_loss = epoch_loss / batch_num
+        print(f'Eopch[{epoch+1}/{epochs}] Average_Loss: {epoch_ave_loss}')
+
+
+
+
+
+
+
+BATCH_SIZE = 256
+EPOCHS = 20
+
+
+INPUT_DIM = 6
+HIDDEN_DIM = 64
+EMBEDDING_DIM = 128
+NUM_LAYERS = 2
+NUM_DIRECTIONS = 2
+DROPOUT = 0.3
+
+LEARNING_RATE = 0.001
+MARGIN = 0.5
+SIMILARITY_WEIGHT = 0.7
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+FILE_PATH = 'puma.txt'
+file_name = FILE_PATH.split('.')[0]
+current_date = datetime.now().strftime('%Y%m%d')
+SAVE_PATH = f'Seq_Embedder_v19_{file_name}_{current_date}.pth'
+
+
+if __name__ == '__main__':
+    
+    print(f'device: {device}')
+
+    dataset = SequenceDataset(FILE_PATH)
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True, 
+        collate_fn=collate_triplet_fn
+    )
+    print(f'Load Data Done! Size: {len(dataset)}')
+
+    model = SequentialEmbedder(
+        INPUT_DIM, 
+        HIDDEN_DIM, 
+        EMBEDDING_DIM, 
+        NUM_LAYERS, 
+        NUM_DIRECTIONS, 
+        DROPOUT
+    ).to(device)
+    print('Model Initialized! ')
+
+    optimizer = torch.optim.Adam(model.parameters(), LEARNING_RATE)
+    criterion = ContractiveLoss(MARGIN, SIMILARITY_WEIGHT)
+    print('Optimizer and Criterion Initialized! ')
+
+    print('Starting Training... ')
+    start_time = time.time()
+    train(model, dataloader, optimizer, criterion, device, EPOCHS)
+    training_time = time.time() - start_time
+    torch.save(model.state_dict(), SAVE_PATH)
+    print(f"Training completed in {training_time//60:.0f}m {training_time%60:.0f}s")
+    print(f"model saved to: {SAVE_PATH}")
